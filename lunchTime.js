6d10434a2a6c2f583f2f9c6da259161b92ec002d
Dennis Amoah <56777642+damoah2@users.noreply.github.com>
2023-10-05 07:13:22 -0400

    Update lunchTime.js

--- lunchTime.js
+++ lunchTime.js
@@ -1,7 +1,6 @@
 var names = ["David", "Alexsa", "Jenny", "May", "Steve"];
 
 function whosPaying() {
-    //Write your code here.
     var randomPerson = Math.floor(Math.random() * names.length)
     console.log(names[randomPerson] + " is going to buy lunch today!");
 }
